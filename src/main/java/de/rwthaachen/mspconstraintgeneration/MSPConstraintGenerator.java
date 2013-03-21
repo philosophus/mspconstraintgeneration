8630d7275d538cfa8a90841d9e60a2d039703eaa
Florian Dahms <florian.dahms@googlemail.com>
2013-03-21 22:41:03 +0100

    Initial commit.
    Add JGraphT to POM
    Add class for msp constraint generation alongside unit test.

---
+++ src/main/java/de/rwthaachen/mspconstraintgeneration/MSPConstraintGenerator.java
@@ -0,0 +1,20 @@
+package de.rwthaachen.mspconstraintgeneration;
+
+import java.util.Set;
+import org.jgrapht.Graph;
+
+/**
+ *
+ * @author florian
+ */
+public class MSPConstraintGenerator {
+
+    public static <V,E> Set<V> generateMSPConstraints(Graph<V,E> graph, Set<V> matchFrom) {
+        
+        
+        
+        System.out.println(graph.toString());
+        
+        return null;
+    }
+}
---
+++ src/test/java/de/rwthaachen/mspconstraintgeneration/MSPConstraintGeneratorTest.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.rwthaachen.mspconstraintgeneration;
+
+import java.util.HashSet;
+import java.util.Set;
+import junit.framework.TestCase;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+/**
+ *
+ * @author florian
+ */
+public class MSPConstraintGeneratorTest extends TestCase {
+    
+    public MSPConstraintGeneratorTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of generateMSPConstraints method, of class MSPConstraintGenerator.
+     */
+    public void testGenerateMSPConstraints() {
+        Set<String> A = new HashSet();
+        Set<String> B = new HashSet();
+        
+        A.add("1");
+        A.add("2");
+        A.add("3");
+        A.add("4");
+        A.add("5");
+        A.add("6");
+        B.add("a");
+        B.add("b");
+        B.add("c");
+        Graph<String, DefaultEdge> graph = new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+        for (String v: A) {
+            graph.addVertex(v);
+        }
+        for (String v: B) {
+            graph.addVertex(v);
+        }
+        
+        graph.addEdge("1", "a");
+        graph.addEdge("2", "a");
+        graph.addEdge("1", "b");
+        graph.addEdge("2", "b");
+        graph.addEdge("3", "b");
+        graph.addEdge("4", "b");
+        graph.addEdge("5", "b");
+        graph.addEdge("6", "b");
+        graph.addEdge("5", "c");
+        graph.addEdge("6", "c");
+                    
+        System.out.println("generateMSPConstraints");
+        Set expResult = null;
+        Set result = MSPConstraintGenerator.generateMSPConstraints(graph, A);
+        assertEquals(expResult, result);
+    }
+}
